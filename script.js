f6c1216588af7e511e6bdb8297d15dd50761410f
dpsiu <denversiu24@gmail.com>
2023-07-17 18:07:59 -0500

    Add feature - delete node func

--- script.js
+++ script.js
@@ -70,10 +70,94 @@ function insert(d, root = this.root) {
     return root;
   }
 
-
 // delete function
-// 
+// Account for 3 cases. When node to delete...
+// Has children, 1 child, or no children
+// If tree is empty, return.
+// Traverse tree until node to delete is found. Once found, log "X found"
+// enter 3 cases.
+// if (root.left == true && root.right == true)
+// if (root.left == true || root.right == true)
+    // if left == true && right != true, left.value = root.value
+// if (root.left && root.right == null)
+// We might simply replace the value? To avoid needing to relink everything.
+// The node to be replaced. Might traverse searching for the noe value
+//  in btwn desired value and parent?
 
+function deleteNode(root, d){
+    if (root == null) {
+        return root
+    }
+
+    if (d > root.d) {
+        root.right.deleteNode(root.right, d)
+    } else if (d < root.d) {
+        root.left.deleteNode(root.left, d)
+    } else {
+        if (root.left == null){
+            return root.right
+        } else if (root.right == null) {
+            return root.left
+        }
+
+        cur = root.right
+        while (cur.left == true) {
+            cur = cur.left
+        }
+        root.d = cur.d
+        root.right = self.deleteNode(root.right, root.d)
+        return root
+    }
+}
+
+function deleteNode(value, root = this.root) {
+    // Base case
+    if (root === null) {
+      return root;
+    }
+
+    // Traverse down the tree
+    if (value < root.data) {
+      root.left = this.deleteNode(value, root.left);
+    } else if (value > root.data) {
+      root.right = this.deleteNode(value, root.right);
+    } 
+
+    // Value matches -> delete node and update pointers
+    else {
+      // option 1: root(child) has only one child
+      if (root.left === null) {
+        // return the child's right so new parent can point to it
+        return root.right;
+      } else if (root.right === null) {
+        // return child's left so new parent can point to it
+        return root.left;
+      }
+      // option 2: Node has two children
+      else {
+        // Replace node with next smallest value
+        const minData = function findNextSmallestRightData(root) {
+          let min = root.data;
+          let newRoot = root;
+
+          // Search for a left node with no left children. 
+          while (newRoot.left !== null) {
+            min = root.left.data;
+            newRoot = root.left;
+          }
+
+          return min;
+        }
+
+        root.data = minData(root.right);
+
+        // Delete the copied node from minData()
+        root.right = this.deleteNode(root.data, root.right)
+      }
+    }
+
+    return root;
+  }
 // find function
 
 // levelOrder func
